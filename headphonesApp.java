6ba76065ca6ecb1d083ea173dd9534ff06273243
Benjamin Borg <info@benjaminb.org>
2022-12-14 18:26:40 +0200

    Added constructors

--- headphonesApp.java
+++ headphonesApp.java
@@ -16,6 +16,13 @@ public class headphonesApp {
         headphones2.printData();
         headphones2.volumeDown();
 
+        System.out.println();
+
+        Headphones headphones3 = new Headphones(true, "Beyerdynamics", "Velour", "Plastic");
+
+        headphones3.printData();
+        
+
     } // End of main
 }
 
@@ -31,12 +38,23 @@ class Headphones // Making a class of headphones, that include the functionality
                 public String material2;
 
                 //Constructors
+
+                //Default constructor - used with headphones1 and headphones2
                 public Headphones ()
                     {
                         isPowered = false;
                         brand = "JBL";
                         material1 = "Pleather";
                         material2 = "Plastic";
+                    }
+
+                //Parameterized constructor - used with headphones3
+                public Headphones (boolean isPowered, String brand, String material1, String material2)
+                    {
+                        this.isPowered = isPowered;
+                        this.brand = brand;
+                        this.material1 = material1;
+                        this.material2 = material2;
                     }
 
                 //Methods
@@ -64,6 +82,8 @@ class Headphones // Making a class of headphones, that include the functionality
                     {
                         System.out.println("Headphones: ");
                         System.out.println("Brand: " + brand);
+                        System.out.println("Cushion-material: " + material1);
+                        System.out.println("Headband-material: " + material2);
                         if (isPowered)
                             {
                                 System.out.println("The headphones are on.");

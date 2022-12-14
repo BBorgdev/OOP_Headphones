27db5c4cea22c5a55a31364c4df3d9fa9c93deb6
Benjamin Borg <info@benjaminb.org>
2022-12-14 17:51:44 +0200

    First commit
    Start of the project

---
+++ headphonesApp.java
@@ -0,0 +1,19 @@
+public class headphonesApp {
+    
+    public static void main(String[] args) {
+        
+    } // End of main
+}
+
+
+class headPhones // Making a class of headphones, that include the functionality of basic headphones, ie. power, volume etc.
+        {
+                //Booleans for power and volume
+                public boolean isPowered;
+                public int volumeLevel;
+
+                //Strings for brand, cushion-material (material1) and headband-material(material2).
+                public String brand;
+                public String material1;
+                public String material2;
+        }
